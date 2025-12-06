4f6c18fd7ec561ad00b15bbaf6e64ac975da5654
Shivam-Joshi-1803 <joshishivam181893@users.noreply.github.com>
2025-12-06 19:01:26 +0530

    Blogs updation

--- components/common/Blogs.tsx
+++ components/common/Blogs.tsx
@@ -6,9 +6,20 @@ import RevealText from "../animation/RevealText";
 import BackgroundParallax from "../animation/BackgroundParallax";
 import AnimatedButton from "../animation/AnimatedButton";
 
-export default function Blogs() {
+// ✅ add this type
+type BlogsProps = {
+  title?: string;
+  desc?: string;
+};
+
+// ✅ accept props here
+export default function Blogs({ title, desc }: BlogsProps) {
   const section = data?.teamPage?.blog?.section;
-  const blogs = data?.teamPage?.blog?.blogs;
+  const blogs = data?.teamPage?.blog?.blogs ?? [];
+
+  // ✅ use props if provided, otherwise fallback to JSON data
+  const sectionTitle = title ?? section?.title ?? "";
+  const sectionDesc = desc ?? section?.desc ?? "";
 
   return (
     <div className="mxd-section padding-blog">
@@ -21,26 +32,30 @@ export default function Blogs() {
                 <div className="col-12 col-xl-5 mxd-grid-item no-margin">
                   <div className="mxd-section-title__hrtitle">
                     <RevealText as="h2" className="reveal-type anim-uni-in-up">
-                      {section.title}
+                      {/* 🔁 use sectionTitle */}
+                      {sectionTitle}
                     </RevealText>
                   </div>
                 </div>
                 <div className="col-12 col-xl-4 mxd-grid-item no-margin">
                   <div className="mxd-section-title__hrdescr">
-                    <p className="anim-uni-in-up">{section.desc}</p>
+                    {/* 🔁 use sectionDesc */}
+                    <p className="anim-uni-in-up">{sectionDesc}</p>
                   </div>
                 </div>
                 <div className="col-12 col-xl-3 mxd-grid-item no-margin">
                   <div className="mxd-section-title__hrcontrols anim-uni-in-up">
-                    <AnimatedButton
-                      text={section.button.text}
-                      className={section.button.class}
-                      href={section.button.href}
-                    >
-                      {section.button.iconClass && (
-                        <i className={section.button.iconClass} />
-                      )}
-                    </AnimatedButton>
+                    {section?.button && (
+                      <AnimatedButton
+                        text={section.button.text}
+                        className={section.button.class}
+                        href={section.button.href}
+                      >
+                        {section.button.iconClass && (
+                          <i className={section.button.iconClass} />
+                        )}
+                      </AnimatedButton>
+                    )}
                   </div>
                 </div>
               </div>
@@ -53,7 +68,7 @@ export default function Blogs() {
           <div className="mxd-blog-preview">
             <div className="container-fluid p-0">
               <div className="row g-0">
-                {blogs.map((item) => (
+                {blogs.map((item: any) => (
                   <div
                     key={item.id}
                     className="col-12 col-xl-4 mxd-blog-preview__item mxd-grid-item animate-card-3"
@@ -76,7 +91,7 @@ export default function Blogs() {
                         </i>
                       </div>
                       <div className="mxd-blog-preview__tags">
-                        {item.tags?.map((tag, tIdx) => (
+                        {item.tags?.map((tag: string, tIdx: number) => (
                           <span
                             key={tIdx}
                             className="tag tag-default tag-permanent"
